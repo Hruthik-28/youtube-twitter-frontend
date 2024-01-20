c5d06a0a47a685e0c936d5a02adf10abbb5720a7
HRUTHIK <hruthik9980819630@gmail.com>
2024-01-20 17:04:31 +0530

    organizing compoenets and icons

---
+++ src/components/icons.js
@@ -0,0 +1,25 @@
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { BiLike } from "react-icons/bi";
+import { HiOutlineVideoCamera } from "react-icons/hi2";
+import { MdOutlineContactSupport } from "react-icons/md";
+import { CiSettings } from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
+import { SlMenu } from "react-icons/sl";
+import { BiHistory } from "react-icons/bi";
+import { TbUserCheck } from "react-icons/tb";
+import { IoFolderOutline } from "react-icons/io5";
+import { RiHome6Line } from "react-icons/ri";
+
+export {
+    IoCloseCircleOutline,
+    BiLike,
+    HiOutlineVideoCamera,
+    MdOutlineContactSupport,
+    CiSettings,
+    CiSearch,
+    SlMenu,
+    BiHistory,
+    IoFolderOutline,
+    RiHome6Line,
+    TbUserCheck
+}
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Button from "./Button";
+import Container from "./Container";
+import Input from "./Input";
+import Login from "./Login";
+import Navbar from "./Header/Navbar";
+import Search from "./Header/Search";
+import Sidebar from "./Header/Sidebar";
+import Logo from "./Logo";
+
+export { Button, Container, Input, Login, Navbar, Search, Sidebar, Logo };
