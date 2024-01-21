c65e63235544d774555c0c6c98def9a448d1b04d
HRUTHIK <hruthik9980819630@gmail.com>
2024-01-21 08:23:29 +0530

    add userSlice

---
+++ src/store/userSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axiosInstance from "../helpers/axiosInstance";
+import toast from "react-hot-toast"
+
+const initialState = {
+    loading: false,
+    profileData: null
+};
+
+export const userChannelProfile = createAsyncThunk('getUserChannelProfile', async(username) => {
+    try {
+        const response = await axiosInstance.get(`/users/c/${username}`);
+        console.log(response);
+        return response.data.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error;
+    }
+});
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(userChannelProfile.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(userChannelProfile.fulfilled, (state, action) => {
+            state.loading = false;
+            state.profileData = action.payload;
+        });
+    },
+});
+
+export default userSlice.reducer
