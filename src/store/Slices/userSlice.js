03229479cd5430c927244ac6c47c5c06b1764550
HRUTHIK <hruthik9980819630@gmail.com>
2024-01-23 18:27:36 +0530

    add updateUserDetails, updateAvatar, updateCoverImg slices

--- src/store/Slices/userSlice.js
+++ src/store/Slices/userSlice.js
@@ -1,27 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../helpers/axiosInstance";
-import toast from "react-hot-toast"
+import toast from "react-hot-toast";
 
 const initialState = {
     loading: false,
     profileData: null,
-    history: []
+    history: [],
 };
 
-export const userChannelProfile = createAsyncThunk('getUserChannelProfile', async(username) => {
-    try {
-        const response = await axiosInstance.get(`/users/c/${username}`);
-        // console.log(response);
-        return response.data.data;
-    } catch (error) {
-        toast.error(error?.response?.data?.error);
-        throw error;
+export const userChannelProfile = createAsyncThunk(
+    "getUserChannelProfile",
+    async (username) => {
+        try {
+            const response = await axiosInstance.get(`/users/c/${username}`);
+            console.log(response);
+            return response.data.data;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
     }
-});
+);
 
-export const getWatchHistory = createAsyncThunk("getWatchHistory", async() => {
+export const getWatchHistory = createAsyncThunk("getWatchHistory", async () => {
     try {
-        const response = await axiosInstance.get('/users/watch-history');
+        const response = await axiosInstance.get("/users/watch-history");
         // console.log(response.data.data);
         return response.data.data;
     } catch (error) {
@@ -29,7 +32,49 @@ export const getWatchHistory = createAsyncThunk("getWatchHistory", async() => {
         toast.error(error?.response?.data?.error);
         throw error;
     }
-})
+});
+
+export const updateUserDetails = createAsyncThunk(
+    "updateUserDetails",
+    async (data) => {
+        try {
+            const response = await axiosInstance("/users/update-user", data);
+            toast.success("Updated details successfully!!!");
+            return response.data.data;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
+    }
+);
+
+export const updateAvatar = createAsyncThunk("updateAvatar", async (avatar) => {
+    try {
+        const response = await axiosInstance("/users/update-avatar", avatar);
+        toast.success("Updated details successfully!!!");
+        return response.data.data;
+    } catch (error) {
+        toast.error(error?.response?.data?.error);
+        throw error;
+    }
+});
+
+export const updateCoverImg = createAsyncThunk(
+    "updateCoverImg",
+    async (coverImage) => {
+        try {
+            const response = await axiosInstance(
+                "/users/update-coverImg",
+                coverImage
+            );
+            toast.success("Updated details successfully!!!");
+            return response.data.data;
+        } catch (error) {
+            toast.error(error?.response?.data?.error);
+            throw error;
+        }
+    }
+);
 
 const userSlice = createSlice({
     name: "user",
@@ -50,7 +95,28 @@ const userSlice = createSlice({
             state.loading = false;
             state.history = action.payload;
         });
+        builder.addCase(updateUserDetails.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(updateUserDetails.fulfilled, (state, action) => {
+            state.loading = false;
+            state.profileData = action.payload;
+        });
+        builder.addCase(updateAvatar.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(updateAvatar.fulfilled, (state, action) => {
+            state.loading = false;
+            state.profileData = action.payload;
+        });
+        builder.addCase(updateCoverImg.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(updateCoverImg.fulfilled, (state, action) => {
+            state.loading = false;
+            state.profileData = action.payload;
+        });
     },
 });
 
-export default userSlice.reducer
+export default userSlice.reducer;
