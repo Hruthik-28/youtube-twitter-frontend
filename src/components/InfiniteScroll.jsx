70a8947261092172be26a2899f82a8e30fe0b608
HRUTHIK <hruthik9980819630@gmail.com>
2024-02-17 15:11:56 +0530

    add custom Infinite Scroll Component

---
+++ src/components/InfiniteScroll.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useEffect, useRef } from "react";
+
+function InfiniteScroll({ children, fetchMore, hasNextPage }) {
+    const loader = useRef(null);
+
+    useEffect(() => {
+        const elementRef = loader.current;
+        const observer = new IntersectionObserver((entries) => {
+            const target = entries[0];
+            if (target.isIntersecting && hasNextPage) {
+                fetchMore();
+            }
+        });
+
+        if (elementRef) observer.observe(elementRef);
+
+        return () => observer.unobserve(elementRef);
+    }, [fetchMore, hasNextPage]);
+
+    return (
+        <>
+            {children}
+            <div ref={loader}></div>
+        </>
+    );
+}
+
+export default InfiniteScroll;
